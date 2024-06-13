bfbe81b56d5ea05195ad3e8e7988482050acc89b
tony <ayakovlenko@users.noreply.github.com>
2024-06-13 23:50:34 +0300

    release: zit 3.1.0-alpha.1

--- zit.rb
+++ zit.rb
@@ -1,8 +1,8 @@
 class Zit < Formula
   desc "Git identity manager"
   homepage "https://github.com/ayakovlenko/zit"
-  url "https://github.com/ayakovlenko/zit/archive/refs/tags/v3.0.1.tar.gz"
-  sha256 "1b595357c0f602ce4d929ddc870ff9cfeffb8c06c55c203afb385d97ad45ef63"
+  url "https://github.com/ayakovlenko/zit/archive/refs/tags/v3.1.0-alpha.1.tar.gz"
+  sha256 "25b710e7ec647713ed73c83b8efebe09009605464b22ee379e3ca9f3ca8ebee4"
 
   depends_on "go" => :build
 
